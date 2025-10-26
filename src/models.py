eb176dd514aa91410c78662d3d6877611469f8b7
Anders Lydig <anders-lk@live.dk>
2025-10-26 23:40:10 +0100

    trained an attention u-net on 582 slices
    should probably try and save models so we don't have to train them every time
    
    should try doing the baseline model on full data (582 slices)
    
    should find a way to run on GPU

--- src/models.py
+++ src/models.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
+# --- Building Block for both U-Net and Attention U-Net ---
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
     def __init__(self, in_channels, out_channels):
@@ -16,6 +18,7 @@ class DoubleConv(nn.Module):
     def forward(self, x):
         return self.double_conv(x)
 
+# --- Baseline U-Net Architecture ---
 class UNet(nn.Module):
     def __init__(self, n_channels, n_classes):
         super(UNet, self).__init__()
@@ -41,3 +44,67 @@ class UNet(nn.Module):
         x = self.conv3(torch.cat([self.up3(x), s2], dim=1))
         x = self.conv4(torch.cat([self.up4(x), s1], dim=1))
         return self.outc(x)
+
+# --- New Attention U-Net Architecture ---
+class AttentionGate(nn.Module):
+    def __init__(self, F_g, F_l, F_int):
+        super(AttentionGate, self).__init__()
+        self.W_g = nn.Sequential(nn.Conv2d(F_g, F_int, kernel_size=1), nn.BatchNorm2d(F_int))
+        self.W_x = nn.Sequential(nn.Conv2d(F_l, F_int, kernel_size=1), nn.BatchNorm2d(F_int))
+        self.psi = nn.Sequential(nn.Conv2d(F_int, 1, kernel_size=1), nn.BatchNorm2d(1), nn.Sigmoid())
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, g, x):
+        g1 = self.W_g(g)
+        x1 = self.W_x(x)
+        psi = self.relu(g1 + x1)
+        psi = self.psi(psi)
+        return x * psi
+
+class AttentionUNet(nn.Module):
+    def __init__(self, n_channels, n_classes):
+        super(AttentionUNet, self).__init__()
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(64, 128))
+        self.down2 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(128, 256))
+        self.down3 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(256, 512))
+        self.down4 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(512, 1024))
+
+        self.up1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.att1 = AttentionGate(F_g=512, F_l=512, F_int=256)
+        self.conv1 = DoubleConv(1024, 512)
+
+        self.up2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.att2 = AttentionGate(F_g=256, F_l=256, F_int=128)
+        self.conv2 = DoubleConv(512, 256)
+
+        self.up3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.att3 = AttentionGate(F_g=128, F_l=128, F_int=64)
+        self.conv3 = DoubleConv(256, 128)
+
+        self.up4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.att4 = AttentionGate(F_g=64, F_l=64, F_int=32)
+        self.conv4 = DoubleConv(128, 64)
+        
+        self.outc = nn.Conv2d(64, n_classes, kernel_size=1)
+
+    def forward(self, x):
+        s1 = self.inc(x); s2 = self.down1(s1); s3 = self.down2(s2); s4 = self.down3(s3); s5 = self.down4(s4)
+        
+        d4 = self.up1(s5)
+        s4 = self.att1(g=d4, x=s4)
+        d4 = self.conv1(torch.cat([d4, s4], dim=1))
+
+        d3 = self.up2(d4)
+        s3 = self.att2(g=d3, x=s3)
+        d3 = self.conv2(torch.cat([d3, s3], dim=1))
+
+        d2 = self.up3(d3)
+        s2 = self.att3(g=d2, x=s2)
+        d2 = self.conv3(torch.cat([d2, s2], dim=1))
+
+        d1 = self.up4(d2)
+        s1 = self.att4(g=d1, x=s1)
+        d1 = self.conv4(torch.cat([d1, s1], dim=1))
+        
+        return self.outc(d1)
